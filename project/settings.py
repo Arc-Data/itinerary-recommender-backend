d9384fc9189fd6d2794b865c1e73687ce6e4e059
Arc <arclight04@gmail.com>
2023-11-21 10:13:27 +0800

    allowed host

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOST = ['*']
+ALLOWED_HOST = ['itinerary-recommender-backend-production.up.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
