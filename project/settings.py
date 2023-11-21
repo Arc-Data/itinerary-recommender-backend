81e48e757703ef9ce5f44675edf5ac90bac4ddb0
Arc <arclight04@gmail.com>
2023-11-21 10:20:27 +0800

    how is this not working

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOST = ['itinerary-recommender-backend-production.up.railway.app']
+ALLOWED_HOSTS = [
+    'itinerary-recommender-backend-production.up.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
