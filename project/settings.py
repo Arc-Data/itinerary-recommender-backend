cf790f82c86f37a984c5894acf6b1d28ec81831e
Arc <arclight04@gmail.com>
2023-11-21 09:43:59 +0800

    switched to postgres

--- project/settings.py
+++ project/settings.py
@@ -18,7 +18,6 @@ from django.conf import settings
 # reading .env file
 env = environ.Env()
 environ.Env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
@@ -129,15 +128,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env("DB_NAME"),
         'USER': env("DB_USER"),
         'PASSWORD': env("DB_PASSWORD"),
         'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
-        'OPTIONS': {
-            'init_command': "SET time_zone='+8:00';"
-        }
     }
 }
 
