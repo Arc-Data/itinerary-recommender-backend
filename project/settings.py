965fadc09e3229a5bfa9245dc858df2b3c9ee9de
Arc <arclight04@gmail.com>
2024-01-08 09:39:24 +0800

    prep for deploying to render

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 import environ
+import dj_database_url
+
 from pathlib import Path
 from datetime import timedelta
 from django.conf import settings
@@ -26,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = [
     'itinerary-recommender-backend-production.up.railway.app',
@@ -140,14 +142,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT")
+        'NAME': 'irsdb',
+        'USER': 'postgres',
+        'PASSWORD': 'mypassword04',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
+database_url = env("DB_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
