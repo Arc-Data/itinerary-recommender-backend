075d6b892d7bb0feaeb72aeba29738fe47ed0d0d
Arc <arclight04@gmail.com>
2024-01-14 17:15:53 +0800

    recustomized review location

--- api/management/commands/review_location.py
+++ api/management/commands/review_location.py
@@ -15,21 +15,26 @@ class Command(BaseCommand):
         location_id = options['location_id']
         rating = options['rating']
 
+        if rating > 5 or rating < 1:
+            self.stderr.write(self.style.ERROR(f"Invalid rating"))
+            return  
+
         try:
             location = Location.objects.get(pk=location_id)
         except Location.DoesNotExist:
             self.stderr.write(self.style.ERROR(f"Location with ID {location_id} does not exist"))
             return
 
-        user_ids = [91, 100, 101, 102, 103]
+        # user_ids = [91, 100, 101, 102, 103]
+        user_ids = [34, 33, 31]
 
         for user_id in user_ids:
             user = User.objects.get(pk=user_id)
 
-            review, created = Review.objects.get_or_create(user=user, location=location, defaults={'rating': 5})
+            review, created = Review.objects.get_or_create(user=user, location=location, defaults={'rating': rating})
             review.comment = f"This is a test review."
             review.rating = rating
             review.datetime_created = timezone.now()
             review.save()
 
-            self.stdout.write(self.style.SUCCESS(f"Review by {user.get_full_name()} created/updated for location '{location.name}' with a 5-star rating"))
+            self.stdout.write(self.style.SUCCESS(f"Review by {user.get_full_name()} created/updated for location '{location.name}' with a {rating}-rating rating"))
